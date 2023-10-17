ba52c00efcba7fe54330ed8e6fbb9cf23d9e7d77
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-10-17 19:30:10 +0300

    Настроить миграции, подключить модели к Alembic

---
+++ app/core/base.py
@@ -0,0 +1,5 @@
+"""
+Импорты класса Base и моделей для Alembic.
+"""
+from app.core.db import Base # noqa
+from app.models import User, CharityProject, Donation # noqa
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.user import User
+from app.models.charityproject import CharityProject
+from app.models.donation import Donation
