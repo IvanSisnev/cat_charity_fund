942705f9495a86b4ae1c78894fe296c07569a451
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-10-17 13:26:36 +0300

    Настроить подключение к БД

--- app/core/db.py
+++ app/core/db.py
@@ -1,1 +1,19 @@
-# todo Base, get_async_session
+"""
+Настройка БД
+"""
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from app.core.config import settings
+
+Base = declarative_base()
+engine = create_async_engine(settings.db_url)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
+
+
+async def get_async_session():
+    """
+    Асинхронный генератор сессий.
+    """
+    async with AsyncSessionLocal() as async_session:
+        yield async_session
