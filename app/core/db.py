4a5e5f72f45c8b331d0b821746b95aa274d82acf
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-10-17 15:43:04 +0300

    Создать расширение базового класса для моделей

--- app/core/db.py
+++ app/core/db.py
@@ -1,13 +1,28 @@
 """
 Настройка БД
 """
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker, declared_attr
 
 from app.core.config import settings
 
-Base = declarative_base()
+
+class PreBase:
+    """
+    Расширение базового класса для всех моделей.
+    """
+    @declared_attr
+    def __tablename__(cls): # noqa
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=PreBase)
+
 engine = create_async_engine(settings.db_url)
+
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
 
 
